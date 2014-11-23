5822a421b9e8e1d3ac898e5bd628c3b3d897d043
Akihiko Sunada <sunada@Akihiko-no-MacBook-Air.local>
2014-11-23 17:06:49 +0900

    dotfiles_link.sh add install neobundle.vim

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -3,3 +3,6 @@
 ln -sf ~/my_git/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/my_git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/my_git/dotfiles/.vimperatorrc ~/.vimperatorrc
+
+mkdir -p .vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
