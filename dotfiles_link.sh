db6775c40291f48d341756f6a2898696a40cdc01
Akihiko <eoncho@gmail.com>
2014-11-25 01:15:50 +0900

    .vrapperrc add

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -3,6 +3,7 @@
 ln -sf ~/my_git/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/my_git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/my_git/dotfiles/.vimperatorrc ~/.vimperatorrc
+ln -sf ~/my_git/dotfiles/.vrapperrc ~/.vrapperrc
 
-mkdir -p .vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+#mkdir -p .vim/bundle
+#git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
