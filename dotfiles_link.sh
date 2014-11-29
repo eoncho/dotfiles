92a0f2cd079dff8abe16ee17eabc49c3b560bdad
Akihiko <eoncho@gmail.com>
2014-11-30 00:21:32 +0900

    dotfiles_link.sh change

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -5,5 +5,9 @@ ln -sf ~/my_git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/my_git/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/my_git/dotfiles/.vrapperrc ~/.vrapperrc
 
-#mkdir -p .vim/bundle
-#git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+if [ -e ~/.vim/bundle ]; then
+    mkdir -p ~/.vim/bundle
+fi
+if [ -f ~/.vim/bundle/neobundle.vim ]; then
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
