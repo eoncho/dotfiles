13351cb6d303a0ac5a4cdf352a168921d7f17fe3
Akihiko <eoncho@gmail.com>
2014-12-01 13:03:07 +0900

    vagrant install add

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -6,34 +6,46 @@ ln -sf ~/my_git/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/my_git/dotfiles/.vrapperrc ~/.vrapperrc
 
 if [ -e ~/.vim/bundle ]; then
-    mkdir -p ~/.vim/bundle
+  mkdir -p ~/.vim/bundle
 fi
 if [ -f ~/.vim/bundle/neobundle.vim ]; then
-    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
 
 if [ $# -gt 0 ]; then
-    if [ $1 = "mac" ]; then
-        if ! which brew > /dev/null ; then
-       	    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-        fi
-        brew update
-        brew upgrade
-        brew tap homebrew/versions
-        brew tap phinze/homebrew-cask
-        brew install brew-cask
-        brew install ctags
-        brew install pyenv-virtualenv
-        brew install rbenv
-        brew install rbenv-gem-rehash
-        brew install rbenv-gemset
-        brew install ruby-build
-       	brew install tmux
-        brew install zsh
-        brew install zsh-completions
-
-        brew cask install java
-        brew cask install eclipse-java
-        brew cask install iterm2
+  if [ $1 = "mac" ]; then
+    if ! which brew > /dev/null ; then
+      ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     fi
+    brew update
+    brew upgrade
+    brew tap homebrew/versions
+    brew tap phinze/homebrew-cask
+    brew install brew-cask
+    brew install ctags
+    brew install pyenv-virtualenv
+    brew install rbenv
+    brew install rbenv-gem-rehash
+    brew install rbenv-gemset
+    brew install ruby-build
+   	brew install tmux
+    brew install zsh
+    brew install zsh-completions
+
+    brew cask install java
+    brew cask install eclipse-java
+    brew cask install iterm2
+    brew cask install sourcetree
+    brew cask install sequel-pro
+    brew cask install vagrant
+    brew cask install virtualbox
+  fi
+fi
+if [ which vagrant > /dev/null ]; then
+  vagrant box add centos64 http://developer.nrel.gov/downloads/vagrant-boxes/CentOS-6.4-x86_64-v20131103.box
+  if [ ! -d ~/Vagrant/CentOS64 ]; then
+    mkdir -p ~/Vagrant/CentOS64
+    cd ~/Vagrant/CentOS64
+    vagrant init
+  fi
 fi
