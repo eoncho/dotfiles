8535b25746f47a749b25ef0f1646a67372a31507
Akihiko <eoncho@gmail.com>
2015-07-17 10:46:43 +0900

    modified r_setting/install.R

--- r_setting/install.R
+++ r_setting/install.R
@@ -1,7 +1,21 @@
-#install.packages("dplyr", repos="http://cran.ism.ac.jp/")
-install.packages("tidyr", repos="http://cran.ism.ac.jp/")
-install.packages("data.table", repos="http://cran.ism.ac.jp/")
-install.packages("DBI", repos="http://cran.ism.ac.jp/")
-install.packages("RMySQL", type='source', repos="http://cran.ism.ac.jp/")
-install.packages("ggplot2", repos="http://cran.ism.ac.jp/")
-install.packages("tseries", repos="http://cran.ism.ac.jp/")
+install_lib <- function(lib) {
+  e <- try(packageVersion(lib))
+  ce <- class(e)
+  if (length(ce) == 1 && ce == "try-error") {
+    install.packages(lib, repos="http://cran.ism.ac.jp/")
+  } else {
+    sprintf("already install [%s ver:%s]",lib, e)
+  }
+}
+
+install_lib("dplyr")
+install_lib("tidyr")
+install_lib("data.table")
+install_lib("foreach")
+install_lib("reshape2")
+install_lib("DBI")
+install_lib("RMySQL")
+install_lib("forecast")
+install_lib("ggplot2")
+install_lib("tseries")
+install_lib("TTR")
