0457d26d4d2d7a2d0aa43d6b95117b03ff5afd8e
Akihiko <eoncho@gmail.com>
2015-10-09 20:29:29 +0900

    modified for mac code

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -12,11 +12,6 @@ for e in ${CONFS[@]}; do
   ln -sf ~/my_git/dotfiles/${e} ~/${e}
 done
 
-APPCONF=( com.googlecode.iterm2.plist )
-for a in ${APPCONF[@]}; do
-  ln -sf ~/my_git/dotfiles/$a ~/Library/Preferences/$a
-done
-
 if [ ! -d ~/.vim/bundle ]; then
   mkdir -p ~/.vim/bundle
 fi
@@ -27,6 +22,11 @@ fi
 # install app
 if [ $# -gt 0 ]; then
   if [ $1 = "mac" ]; then
+    APPCONF=( com.googlecode.iterm2.plist )
+    for a in ${APPCONF[@]}; do
+      ln -sf ~/my_git/dotfiles/$a ~/Library/Preferences/$a
+    done
+
     if ! which brew > /dev/null ; then
       ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     fi
@@ -104,52 +104,56 @@ if [ $# -gt 0 ]; then
     fi
     npm i -g archy
     npm i -g yo generator-hubot
+
+    if ! vim --version | grep "+clipboard" > /dev/null; then
+      sudo mv /usr/bin/vim /usr/bin/old_vim
+      sudo ln /usr/local/Cellar/vim/`ls /usr/local/Cellar/vim/`/bin/vim /usr/bin
+    fi
+    #  nfsd setting
+    if ! sudo nfsd status | grep "enable" ; then
+      sudo nfsd enable
+      sudo touch /etc/exports
+    fi
+    # vagrant setting
+    if which vagrant > /dev/null ; then
+      if ! vagrant box list | grep centos64_dev_base > /dev/null; then
+        vagrant box add centos64_dev_base ~/Vagrant/boxs/centos64_dev_base_20141229.box
+      fi
+      if ! vagrant plugin list | grep vagrant-omnibus > /dev/null; then
+        vagrant plugin install vagrant-omnibus
+      fi
+      if ! vagrant plugin list | grep vagrant-berkshelf > /dev/null; then
+        vagrant plugin install vagrant-berkshelf
+      fi
+    fi
+    # ruby setting
+    if ! rbenv versions | grep 2.1.0 > /dev/null; then
+      rbenv install 2.1.0
+      rbenv global 2.1.0
+    fi
+    GEMS=()
+    GEMS+=( bundler )
+    GEMS+=( pry )
+    GEMS+=( twterm )
+
+    for g in ${GEMS[@]}; do
+      if ! gem list | grep $g > /dev/null; then
+        gem install $g
+      fi
+    done
+    if ! gem list | grep nokogiri > /dev/null; then
+      gem install nokogiri -- --use-system-libraries --with-iconv-dir="$(brew --prefix libiconv)" --with-xml2-config="$(brew --prefix libxml2)/bin/xml2-config" --with-xslt-config="$(brew --prefix libxslt)/bin/xslt-config"
+    fi
+    # embulk setting
+    if ! which embulk > /dev/null ; then
+      curl --create-dirs -o ~/.embulk/bin/embulk -L https://bintray.com/artifact/download/embulk/maven/embulk-0.6.1.jar
+      chmod +x ~/.embulk/bin/embulk
+    fi
+
   fi
 fi
+
 # vim setting
 if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
   git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-fi
-if ! vim --version | grep "+clipboard" > /dev/null; then
-  sudo mv /usr/bin/vim /usr/bin/old_vim
-  sudo ln /usr/local/Cellar/vim/`ls /usr/local/Cellar/vim/`/bin/vim /usr/bin
-fi
-#  nfsd setting
-if ! sudo nfsd status | grep "enable" ; then
-  sudo nfsd enable
-  sudo touch /etc/exports
-fi
-# vagrant setting
-if which vagrant > /dev/null ; then
-  if ! vagrant box list | grep centos64_dev_base > /dev/null; then
-    vagrant box add centos64_dev_base ~/Vagrant/boxs/centos64_dev_base_20141229.box
-  fi
-  if ! vagrant plugin list | grep vagrant-omnibus > /dev/null; then
-    vagrant plugin install vagrant-omnibus
-  fi
-  if ! vagrant plugin list | grep vagrant-berkshelf > /dev/null; then
-    vagrant plugin install vagrant-berkshelf
-  fi
-fi
-# ruby setting
-if ! rbenv versions | grep 2.1.0 > /dev/null; then
-  rbenv install 2.1.0
-  rbenv global 2.1.0
-fi
-GEMS=()
-GEMS+=( bundler )
-GEMS+=( pry )
-GEMS+=( twterm )
-for g in ${GEMS[@]}; do
-  if ! gem list | grep $g > /dev/null; then
-    gem install $g
-  fi
-done
-if ! gem list | grep nokogiri > /dev/null; then
-  gem install nokogiri -- --use-system-libraries --with-iconv-dir="$(brew --prefix libiconv)" --with-xml2-config="$(brew --prefix libxml2)/bin/xml2-config" --with-xslt-config="$(brew --prefix libxslt)/bin/xslt-config"
-fi
-# embulk setting
-if ! which embulk > /dev/null ; then
-  curl --create-dirs -o ~/.embulk/bin/embulk -L https://bintray.com/artifact/download/embulk/maven/embulk-0.6.1.jar
-  chmod +x ~/.embulk/bin/embulk
 fi
