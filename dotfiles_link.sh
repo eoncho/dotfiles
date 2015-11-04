f25c98cb831b69fa5a02ce1a2c28ca475f85df87
Akihiko <eoncho@gmail.com>
2015-11-04 10:41:46 +0900

    delete ansible install

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -67,7 +67,6 @@ if [ $# -gt 0 ]; then
     brew install automake
     brew install e2fsprogs
     brew install terraform
-    brew install ansible
 
     brew cask install osxfuse
     brew cask install java
